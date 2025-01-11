a792667773631c49f0a18869d795da2e87ac05e8
Jakub Czura <czurajakub@gmail.com>
2025-01-11 16:06:31 +0100

    Add authorization with Auth0 to wasm client app

--- ApplicationClient/Program.cs
+++ ApplicationClient/Program.cs
@@ -1,11 +1,36 @@
 using ApplicationClient;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new AuthorizationMessageHandler(sp.GetRequiredService<IAccessTokenProvider>(),
+                                                                 sp.GetRequiredService<NavigationManager>())
+                .ConfigureHandler(["https://localhost:7071"]));
+
+builder.Services.AddHttpClient("ApiGateway", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7071"); // Address of API Gateway
+}).AddHttpMessageHandler<AuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ApiGateway"));
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+
+    options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration["Auth0:Audience"]!);
+
+    options.ProviderOptions.ResponseType = "code";
+
+    options.ProviderOptions.DefaultScopes.Add("openid");
+    options.ProviderOptions.DefaultScopes.Add("profile");
+    options.ProviderOptions.DefaultScopes.Add("email");
+    options.ProviderOptions.DefaultScopes.Add("offline_access");
+});
 
 await builder.Build().RunAsync();
