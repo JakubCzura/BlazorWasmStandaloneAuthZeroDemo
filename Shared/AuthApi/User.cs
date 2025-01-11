71d2f2d1ef1d37e5880092214b7488eec187d199
Jakub Czura <czurajakub@gmail.com>
2025-01-11 15:16:44 +0100

    Add Shared project

---
+++ Shared/AuthApi/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.AuthApi;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+}
